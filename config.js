3c224325069b156eb471e7b5c025e32b66a739c6
IGREEDKSA <48129328+IGREEDKSA@users.noreply.github.com>
2019-03-01 19:14:22 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     TOKEN: 'Your bot token here', 
-    YT_API_KEY: 'Youtube API Key here', 
-    prefix: 'prefix here',
-    devs: ['your id/developers here']
+    YT_API_KEY: 'AIzaSyCQapWI28_Um0n4AEGNVw7l3QZqhQF0cFs', 
+    prefix: 'A!',
+    devs: ['305061160062025728']
 }
