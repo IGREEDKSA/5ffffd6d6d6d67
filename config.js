cd7a706d2111009c8e0c07220a6fba05c82f1344
IGREEDKSA <48129328+IGREEDKSA@users.noreply.github.com>
2019-03-01 19:54:22 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    TOKEN: 'Your bot token here', 
+    TOKEN: 'NTUwOTQ2NzE0NTQzMDYzMDYw.D1r1Bg.erLBXhKKhofUFmtfXT7J-AmYVXk', 
     YT_API_KEY: 'AIzaSyCQapWI28_Um0n4AEGNVw7l3QZqhQF0cFs', 
     prefix: 'A!',
     devs: ['305061160062025728']
