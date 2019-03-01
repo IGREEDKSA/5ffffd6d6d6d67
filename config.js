e7ec1069e130dd2561694d383d7cb70966ba3343
IGREEDKSA <48129328+IGREEDKSA@users.noreply.github.com>
2019-03-01 20:17:29 +0300

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    TOKEN: 'NTUwOTQ2NzE0NTQzMDYzMDYw.D1r1Bg.erLBXhKKhofUFmtfXT7J-AmYVXk', 
-    YT_API_KEY: 'AIzaSyCQapWI28_Um0n4AEGNVw7l3QZqhQF0cFs', 
-    prefix: 'A!',
-    devs: ['305061160062025728']
-}
